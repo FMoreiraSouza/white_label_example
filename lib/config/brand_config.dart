bd44a3eac99a8b37ad80d0a75330f4f42b9adb32
FMoreiraSouza <fmoreirasouza701@gmail.com>
2025-02-10 12:21:40 -0300

    first commit

---
+++ lib/config/brand_config.dart
@@ -0,0 +1,27 @@
+﻿import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BrandConfig {
+  final String appName;
+  final String logoPath;
+  final ThemeData theme;
+
+  BrandConfig({
+    required this.appName,
+    required this.logoPath,
+    required this.theme,
+  });
+
+  static Future<BrandConfig> loadBrandConfig(String brand) async {
+    final jsonString = await rootBundle.loadString('assets/brands/$brand/config.json');
+    final jsonMap = json.decode(jsonString);
+
+    return BrandConfig(
+      appName: jsonMap['appName'],
+      logoPath: jsonMap['logoPath'],
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
---
+++ lib/config/flavor_config.dart
@@ -0,0 +1,11 @@
+﻿import 'package:white_label_example/config/brand_config.dart';
+
+class FlavorConfig {
+  static late BrandConfig _brandConfig;
+
+  static void setBrand(BrandConfig config) {
+    _brandConfig = config;
+  }
+
+  static BrandConfig get brand => _brandConfig;
+}
